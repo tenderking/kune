1864025844dfbddd231299d55c5b7677205248df
George Mushore <gmu003@gmail.com>
2022-01-06 02:14:47 +0100

    design changes home page

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,45 @@
 <template>
-
-   <header>
-    <router-link 
-      class="logo" 
-      @click="isActive = false" 
-      to="/"> Kune 
+  <header>
+    <router-link class="logo" @click="isActive = false" to="/">
+      Kune
     </router-link>
     <nav>
       <!-- Left-aligned links -->
-      <ul>
+      <ul class="nav-small-screen" >
+        <li>
+          <router-link @click="isActive = false" to="/services"
+            >Browse Services
+          </router-link>
+        </li>
 
-      <li>
-        <router-link @click="isActive = false" to="/sign-in"
-          >sign in</router-link
-        >
-      </li>
-      <li><a  @click="toggleClass()" v-if="isActive===false" >menu</a>  <fa  v-else icon="times" @click="toggleClass()"   /></li>
+        <li>
+          <router-link @click="isActive = false" to="/about">About</router-link>
+        </li>
+        <li>
+          <router-link @click="isActive = false" to="#">Contact us</router-link>
+        </li>
+        <li>
+          <router-link @click="isActive = false" to="/sign-in"
+            >Sign in</router-link
+          >
+        </li>
+       
       </ul>
+          <a class="bars-menu" @click="toggleClass()" v-if="isActive === false"
+            ><fa icon="bars"
+          /></a>
+          <fa class="bars-menu"  v-else icon="times" @click="toggleClass()" />
     </nav>
   </header>
-   <!-- Drop-down menu -->
+  <!-- Drop-down menu -->
+  <transition name="fade">
+
   <div class="menu-container" :class="{ 'menu-container-active': isActive }">
-  
     <div class="bar">
-      <router-link 
-        @click="isActive = false" 
-        to="/services"
+      <router-link @click="isActive = false" to="/sign-in">Sign in</router-link>
+    </div>
+    <div class="bar">
+      <router-link @click="isActive = false" to="/services"
         >Browse Services
       </router-link>
     </div>
@@ -36,23 +50,21 @@
       <router-link @click="isActive = false" to="#">Contact us</router-link>
     </div>
   </div>
+  </transition>
 </template>
 
 <script>
 import { ref } from "vue";
 export default {
-    name: "NavBar",
+  name: "NavBar",
   setup() {
-    
     const isActive = ref(false);
     const toggleClass = () => {
       isActive.value = !isActive.value;
     };
     return { isActive, toggleClass };
   },
-  }
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,7 +22,33 @@
 
       <!-- second section -->
 
+
+
+      <section class="motivation-section">
+        <div class="motivation-card">
+          <h3> Digital</h3>
+          Our focus is promoting a digital Zimbabwe. More and more people are starting to use internet, and in the vastness of the internet, we  provide the opportunity for a rich experience.
+        </div>
+        <div class="motivation-card">
+          <h3> Discover </h3>
+          At <span class="kune"> kune </span>we showcase a vast number of Zimbabwean
+          websites, what type of services they offer and where they a located.
+          You can easily bookmark the services you find interesting if you want
+          to come back to them
+        </div>
+        <div class="motivation-card">
+           <h3> Advertise </h3>
+          <p>
+            Are you a business owner and looking for the place to showcase your
+            business and attract customers. At
+            <span class="kune"> kune </span> you can do exaclty just that.
+          </p>
+        </div>
+      </section>
+
+      <!-- third section -->
       <section class="category-picker">
+       
         <p class="category-picker-text">
           Find the best local businesses and services in your area
         </p>
@@ -40,24 +66,6 @@
               <fa class="chevron-right" icon="chevron-right" />
             </router-link>
           </div>
-        </div>
-      </section>
-
-      <!-- third section -->
-
-      <section class="motivation-section">
-        <div class="motivation-card-1">
-          <p>
-            Are you a business owner and looking for the place to showcase your
-            business and attract customers. At
-            <span class="kune"> kune </span> you can do exaclty just that.
-          </p>
-        </div>
-        <div class="motivation-card-2">
-          At <span class="kune"> kune </span> a vast number of Zimbabwean
-          websites, what type of services they offer and where they a located.
-          You can easily bookmark the services you find interesting if you want
-          to come back to them
         </div>
       </section>
   
